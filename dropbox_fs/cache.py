267c649c6c1dd959cec45d8d1b6429f03cb58d6f
Jan Hammerschmidt <jan.hammerschmidt@gmail.com>
2018-09-20 21:22:35 +0200

    handle connection problems

--- dropbox_fs/cache.py
+++ dropbox_fs/cache.py
@@ -2,9 +2,11 @@ import contextlib
 import logging
 import errno
 from typing import List
+from enum import Enum
 from pathlib import Path
 from threading import Thread, Event
 from fuse import FuseOSError
+from requests.exceptions import ReadTimeout
 from dropbox import Dropbox
 from dropbox_fs.crawler import File
 
@@ -18,9 +20,14 @@ class SizeWatcher(Event):
 
 
 class FileDownloader:
+    class State(Enum):
+        working = 0
+        success = 1
+        failure = 2
+
     def __init__(self, path: str, file: Path, dbx: Dropbox, db_path: str, finished_callback):
         self.path, self.file, self.dbx, self.db_path = path, file, dbx, db_path
-        self.successfully_finished = False
+        self.state = self.State.working
         self.finished_callback = finished_callback
         self.size_watcher: List[SizeWatcher] = []
 
@@ -33,32 +40,41 @@ class FileDownloader:
         log.debug('downloading {}'.format(self.db_path))
         with contextlib.closing(self):
             with contextlib.closing(self.f):
-                md, res = self.dbx.files_download(self.db_path)
-                with contextlib.closing(res):
-                    for c in res.iter_content(2 ** 16):
-                        self.f.write(c)
-                        self.bytes_downloaded += len(c)
-                        [w.set() for w in self.size_watcher if self.bytes_downloaded > w.size]
+                try:
+                    md, res = self.dbx.files_download(self.db_path)
+                    with contextlib.closing(res):
+                        for c in res.iter_content(2 ** 16):
+                            self.f.write(c)
+                            self.bytes_downloaded += len(c)
+                            [w.set() for w in self.size_watcher if self.bytes_downloaded > w.size]
+                except (ConnectionError, ReadTimeout):
+                    log.error('downloading failed for {}'.format(self.db_path))
+                    self.state = self.State.failure
+                    [w.set() for w in self.size_watcher]
+                    return
 
-            self.successfully_finished = True
+            self.state = self.State.success
             log.debug('download finished: {}'.format(self.db_path))
             [w.set() for w in self.size_watcher]
 
     def close(self):
         self.finished_callback(self)
 
-    def wait_for_size(self, size):
+    def wait_for_size(self, size) -> bool:
         log.debug('waiting for size {}: {}'.format(size, self.db_path))
-        if not self.successfully_finished and self.bytes_downloaded < size:
+        if self.state == self.State.working and self.bytes_downloaded < size:
             log.debug('new size watcher for {}'.format(self.db_path))
             watcher = SizeWatcher(size)
             self.size_watcher.append(watcher)
-            while self.bytes_downloaded < size and not self.successfully_finished:
+            while self.bytes_downloaded < size and self.state == self.State.working:
                 watcher.wait(2)
             self.size_watcher.remove(watcher)
-        log.debug('flush {}'.format(self.db_path))
+        if self.state == self.State.failure:
+            return False
         if not self.f.closed:
+            log.debug('flush {}'.format(self.db_path))
             self.f.flush()
+        return True
 
 
 class FileCache:
@@ -88,7 +104,8 @@ class FileCache:
             log.error('no open file found while reading from {}'.format(path))
             raise FuseOSError(errno.EIO)
         if path in self.downloading:
-            self.downloading[path].wait_for_size(offset+size)
+            if not self.downloading[path].wait_for_size(offset+size):
+                raise FuseOSError(errno.EIO)
         # else:
         #     log.debug('{} not in {}'.format(path, list(self.downloading.keys())))
         f.seek(offset)
@@ -102,7 +119,6 @@ class FileCache:
 
     def finished_downloading(self, downloader: FileDownloader):
         log.debug('removing {} from downloading'.format(downloader.db_path))
-        assert downloader.successfully_finished
         del self.downloading[downloader.path]
 
     def open_file(self, file, _flags) -> int:
